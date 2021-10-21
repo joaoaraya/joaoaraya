c65c8fde9ce0d4670a87f77b611edd9d76361f99
Araya <joaoaraya7@gmail.com>
2021-10-20 22:52:57 -0300

    paginas adcionadas ao index

---
+++ src/functions/tabs.js
@@ -0,0 +1,22 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+    // Scroll to top
+    document.documentElement.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
