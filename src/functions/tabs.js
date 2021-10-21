1e7131c1320e4292a6cb63938580be5a7a693bae
Araya <joaoaraya7@gmail.com>
2021-10-20 23:48:30 -0300

    correções no titulo das paginas em telas pequenas

--- src/functions/tabs.js
+++ src/functions/tabs.js
@@ -1,4 +1,6 @@
-function openTab(evt, tabName) {
+let tabTop = document.querySelector('.tab-title');
+
+function openTab(evt, tabName, named) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -11,8 +13,11 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 
+    // Nome da tab
+    tabTop.innerHTML = `<p>${named}</p>`;
+
     // Scroll to top
-    document.documentElement.scrollTo({
+    tabTop.scrollIntoView({
         top: 0,
         behavior: "smooth"
     });
